a0f076efcb75f2bc83b1fc0beaaacf26b5226189
Lming1 <spear131121@gmail.com>
2018-07-16 14:07:52 +0900

    node express -> spring

---
+++ solutioncoding/src/main/java/com/newone/solutioncoding/SolutioncodingApplication.java
@@ -0,0 +1,12 @@
+package com.newone.solutioncoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutioncodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutioncodingApplication.class, args);
+    }
+}
