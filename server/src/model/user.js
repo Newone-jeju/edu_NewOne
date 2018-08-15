d0415e700f5f85768b2000235a501fbf763ceb60
warmboi <warm_boi@naver.com>
2018-08-15 13:04:55 +0900

    user model updated

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,9 +1,18 @@
 module.exports = (sequelize, dataType) => {
   const user = sequelize.define('user', {
     id: {
+      type: dataType.INTEGER(),
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
       type: dataType.STRING(16),
       primaryKey: true,
       allowNull: false
+    },
+    password: {
+      type: dataType.STRING(64),
+      allowNull: true
     },
     name: {
       type: dataType.STRING(16),
