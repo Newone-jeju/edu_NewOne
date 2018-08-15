0c5e1fc51a522008241ebc0df1c3d995131a218c
warmboi <warm_boi@naver.com>
2018-08-15 12:59:09 +0900

    login with jwt

--- server/src/router/auth_router.js
+++ server/src/router/auth_router.js
@@ -1,7 +1,5 @@
+const auth = require('../controllers/auth_controller')
 module.exports = (app) => {
   app.route('/login')
-    .post(function (req, res) {
-      console.log(!11111)
-      res.send(req.body)
-    })
+    .post(auth.login)
 }
