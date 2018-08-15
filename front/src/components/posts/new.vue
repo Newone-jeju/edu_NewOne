41aebb01c98e98f883d3489ed8f0eef10b63aded
ojlee <dldhwn56@gmail.com>
2018-08-15 15:20:45 +0900

    logout

--- front/src/components/posts/new.vue
+++ front/src/components/posts/new.vue
@@ -1,9 +1,17 @@
 <template>
-<div id="board" class="container">
-<input v-model="title"/>
-<input v-model="content"/>
-<button @click="write">write</button>
-</div>
+  <div id="board" class="container">
+    <div class="main-section">
+      <div class="grid-container">
+        <div class="row">
+          <div class="col-lg-20 col-xl-20">
+            <input v-model="title"/>
+            <input v-model="content"/>
+            <button @click="write">write</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
