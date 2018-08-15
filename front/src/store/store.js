563ecd0a42fcf9f5c6366b600631c1fc12991aea
warmboi <warm_boi@naver.com>
2018-08-15 15:23:35 +0900

    store cookie

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersisedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie'
 
 Vue.use(Vuex)
 
@@ -38,6 +39,13 @@ export default new Vuex.Store({
     }
   },
   plugins: [
-    createPersisedState()
+    createPersisedState({
+      storage: {
+        getItem: key => Cookies.get(key),
+        // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+        setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
+        removeItem: key => Cookies.remove(key)
+      }
+    })
   ]
 })
